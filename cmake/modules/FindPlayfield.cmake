cb2bf0a4074be00cc15e7b5a53401968f40d055e
Awe Morris <awe@noctvm.io>
2026-05-08 19:57:03 +0900

    Fix FindPlayfield.cmake

--- cmake/modules/FindPlayfield.cmake
+++ cmake/modules/FindPlayfield.cmake
@@ -3,7 +3,7 @@ find_path(PLAYFIELD_INCLUDE_DIR playfield/playfield.h)
 find_library(PLAYFIELD_LIBRARY NAMES playfield)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(PLAYFIELD DEFAULT_MSG PLAYFIELD_INCLUDE_DIR)
+find_package_handle_standard_args(Playfield DEFAULT_MSG PLAYFIELD_INCLUDE_DIR)
 
 set(PLAYFIELD_INCLUDE_DIRS ${PLAYFIELD_INCLUDE_DIR})
 set(PLAYFIELD_LIBRARIES ${PLAYFIELD_LIBRARY})
